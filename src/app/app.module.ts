57e6fa31385f1b2a52b7fbb835bc419ca7e698e2
lmseo <luis@lmseo.com>
2017-11-28 14:51:32 -0800

    remove old files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,6 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppComponent } from './app.component';
 import { UsernameComponent } from './username/username.component';
-import { SecrerpasswordComponent } from './secrerpassword/secrerpassword.component';
-import { HeaderComponent } from './header/header.component';
-import { HeaderngxComponent } from './headerngx/headerngx.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { RollDicesComponent } from './roll-dices/roll-dices.component';
 import { SignupComponent } from './signup/signup.component';
 
 
@@ -21,14 +13,6 @@ import { SignupComponent } from './signup/signup.component';
   declarations: [
     AppComponent,
     UsernameComponent,
-    SecrerpasswordComponent,
-    HeaderComponent,
-    HeaderngxComponent,
-    RecipesComponent,
-    RecipeDetailComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RollDicesComponent,
     SignupComponent
   ],
   imports: [
